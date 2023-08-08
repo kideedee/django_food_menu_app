543638ac8b0ee767378a8667b94517749db41df7
sonvq <kideedee1234@gmail.com>
2023-08-08 11:07:05 +0700

    adding form to add items

--- food/views.py
+++ food/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from food.forms import ItemForm
 from food.models import Item
 
 
@@ -24,3 +25,12 @@ def detail(request, item_id):
     }
 
     return render(request, 'food/detail.html', context)
+
+
+def create_item(request):
+    form = ItemForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+
+    return render(request, 'food/item-form.html', {'form': form})
