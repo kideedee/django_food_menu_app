023b34a83a3d1150ba1836efd8a8305a4b48c487
sonvq <kideedee1234@gmail.com>
2023-08-08 15:27:34 +0700

    creating user registration form

--- FoodMenuApp/urls.py
+++ FoodMenuApp/urls.py
@@ -19,7 +19,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from users import views as user_views
+
 urlpatterns = [
                   path('admin/', admin.site.urls),
-                  path('food/', include('food.urls'))
+                  path('food/', include('food.urls')),
+                  path('register', user_views.register, name='register')
               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,7 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
 
-# Create your views here.
+
+def register(request):
+    form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
