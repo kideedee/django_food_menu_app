0b187b034bc0b5872d8b3709b419925519f20cce
sonvq <kideedee1234@gmail.com>
2023-08-09 15:05:57 +0700

    setting up the default profile picture

--- FoodMenuApp/urls.py
+++ FoodMenuApp/urls.py
@@ -22,12 +22,12 @@ from django.urls import path, include
 
 from users import views as user_views
 
-urlpatterns = \
-    [
-        path('admin/', admin.site.urls),
-        path('food/', include('food.urls')),
-        path('register/', user_views.register, name='register'),
-        path('login/', authentication_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-        path('logout/', authentication_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-        path('profile/', user_views.profile_page, name='profile')
-    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('food/', include('food.urls')),
+    path('register/', user_views.register, name='register'),
+    path('login/', authentication_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', authentication_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('profile/', user_views.profile_page, name='profile')
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
