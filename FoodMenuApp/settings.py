b843cf3eb50ad44faaacc4f29ccb4088a4191f73
sonvq <kideedee1234@gmail.com>
2023-08-09 11:55:31 +0700

    adding path to upload images

--- FoodMenuApp/settings.py
+++ FoodMenuApp/settings.py
@@ -124,3 +124,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'food:index'
 LOGIN_URL = 'login'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'pictures')
+MEDIA_URL = '/pictures/'
